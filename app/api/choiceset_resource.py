da3aa87456dc8c2aeedf4732a246abdf61e4c87e
emilmirzayev <mirzayev_emil@yahoo.com>
2022-09-02 18:12:18 +0100

    changed choice set generation to random for repeated IP addresses

--- app/api/choiceset_resource.py
+++ app/api/choiceset_resource.py
@@ -14,24 +14,18 @@ class ChoiceSetResource(MethodView):
     def post(self):
         data = request.get_json()
         columns = "F1 F2 F3 F4 F5".split()
-        # check first if such choice set for that task id exists. if yes, return it. if not, create it
-        exists = db.session.query(
-                    db.session.query(ChoiceSets).filter_by(task_id = data["task_id"]).exists()
-        ).scalar()
 
-        if exists:
-            return jsonify({"Message": "Choice set already exists for this task_id, returning", 'columns''': columns, 'choice_set': self.get_choice_sets(data["task_id"])})
-        else:
+
                 
-            df = pd.DataFrame(np.random.randint(0, 500, size = (100, 5)))
-            
-            df.columns = columns
-            df["objective_score"] = df["F1"] + df["F3"] + df["F5"]
-            df["task_id"] = data["task_id"]
-            records = df.to_dict(orient = "records")
-            # for record in records:
-            #     ChoiceSets.create(**record)
-            db.engine.execute(ChoiceSets.__table__.insert(), records)
+        df = pd.DataFrame(np.random.randint(0, 500, size = (100, 5)))
+        
+        df.columns = columns
+        df["objective_score"] = df["F1"] + df["F3"] + df["F5"]
+        df["task_id"] = data["task_id"]
+        records = df.to_dict(orient = "records")
+        # for record in records:
+        #     ChoiceSets.create(**record)
+        db.engine.execute(ChoiceSets.__table__.insert(), records)
         return jsonify({'columns''': columns, 'choice_set': self.get_choice_sets(data["task_id"])})
 
     def get(self):
